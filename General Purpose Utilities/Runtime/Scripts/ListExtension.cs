aa7006844faad28e2cdcdfeb95bd66bda40acca0
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2022-11-29 20:35:32 +0300

    Добавлен метод расширения списков MoveItemFromTo
    + Метод перемещает элемент на указанную позицию.

---
+++ General Purpose Utilities/Runtime/Scripts/ListExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace IUP.Toolkits
+{
+    public static class ListExtension
+    {
+        /// <summary>
+        /// Перемещает элемент на позицию переданного индекса, передвигая все остальные элементы.
+        /// </summary>
+        /// <param name="from">Индекс передвигаемого элемента.</param>
+        /// <param name="to">Индекс, на который передвигается элемент.</param>
+        public static void MoveItemFromTo<T>(this List<T> list, int from, int to)
+        {
+            T movedItem = list[from];
+            list.RemoveAt(from);
+            list.Insert(to, movedItem);
+        }
+    }
+}
