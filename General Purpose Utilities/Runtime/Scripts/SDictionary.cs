26b8cf40aef774b274967e1d686c72e5da2d24b5
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2022-12-11 05:37:25 +0300

    Добавлен сериализуемый словарь
    + Создан класс сериализуемого словаря, доступный для изменения через инспектор.

---
+++ General Purpose Utilities/Runtime/Scripts/SDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IUP.Toolkits
+{
+    /// <summary>
+    /// Сериализуемый словарь, отображаемый в инспекторе Unity.
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключей словаря.</typeparam>
+    /// <typeparam name="TValue">Тип значений словаря.</typeparam>
+    [Serializable]
+    public sealed class SDictionary<TKey, TValue> : ISerializationCallbackReceiver
+    {
+        /// <summary>
+        /// Словарь.
+        /// </summary>
+        public IReadOnlyDictionary<TKey, TValue> Value => _value;
+
+        private Dictionary<TKey, TValue> _value;
+        [SerializeField] private List<SKeyValuePair<TKey, TValue>> _pairs;
+
+        /// <summary>
+        /// Не использовать, если не знаете, что делаете. Метод обратного вызова после десериализации.
+        /// </summary>
+        public void OnAfterDeserialize()
+        {
+            _value = new Dictionary<TKey, TValue>(_pairs.Count);
+            for (int i = 0; i < _pairs.Count; i += 1)
+            {
+                if (!_value.ContainsKey(_pairs[i].Key))
+                {
+                    _value.Add(_pairs[i].Key, _pairs[i].Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Не использовать, если не знаете, что делаете. Метод обратного вызова перед сериализацией.
+        /// </summary>
+        public void OnBeforeSerialize() { }
+    }
+}
---
+++ General Purpose Utilities/Runtime/Scripts/SKeyValuePair.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+namespace IUP.Toolkits
+{
+    /// <summary>
+    /// Пара ключ-значение для десериализуемого словаря.
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключа пары.</typeparam>
+    /// <typeparam name="TValue">Тип значения пары.</typeparam>
+    [Serializable]
+    public class SKeyValuePair<TKey, TValue>
+    {
+        public SKeyValuePair(TKey key = default, TValue value = default)
+        {
+            _key = key;
+            _value = value;
+        }
+
+        /// <summary>
+        /// Ключ.
+        /// </summary>
+        public TKey Key => _key;
+        /// <summary>
+        /// Значение.
+        /// </summary>
+        public TValue Value => _value;
+
+        [SerializeField] private TKey _key;
+        [SerializeField] private TValue _value;
+    }
+}
