528062d756c4d0920c024a32bafc2a67f48a1541
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2023-03-26 16:31:06 +0300

    Добавлен метод поворота Vector2

--- IUP General Purpose Utilities/Runtime/Scripts/ExtendedMath.cs
+++ IUP General Purpose Utilities/Runtime/Scripts/ExtendedMath.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace IUP.Toolkits
 {
@@ -7,6 +8,13 @@ namespace IUP.Toolkits
         public static double RoundWithStep(double number, double step)
         {
             return Math.Round(number / step) * step;
+        }
+
+        public static Vector2 RotateVector2(Vector2 vector, float rotationInRadian)
+        {
+            float x = (vector.x * Mathf.Cos(rotationInRadian)) - (vector.y * Mathf.Sin(rotationInRadian));
+            float y = (vector.x * Mathf.Sin(rotationInRadian)) + (vector.y * Mathf.Cos(rotationInRadian));
+            return new Vector2(x, y);
         }
     }
 }
